674d5ba0e1e5c81bf372df8b6fb58c4a1bfe8577
wrfmartinez <wrmartinez709@gmail.com>
2024-06-06 11:15:01 -0400

    Configure CORS and add plural for models

--- django_rest_api/companies_api/models.py
+++ django_rest_api/companies_api/models.py
@@ -3,9 +3,13 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=100)
     industry = models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = "Companies"
 
 class Location(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='locations')
+    class Meta:
+        verbose_name_plural = "Locations"
--- django_rest_api/django_rest_api/settings.py
+++ django_rest_api/django_rest_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'companies_api',
     'django.contrib.admin',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'django_rest_api.urls'
 
